dad92a14cb70e61afe9ab32fc4cf91478741d794
Regina Iuldasheva <reginaiuldasheva@gmail.com>
2023-03-19 17:36:28 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+const board = document.querySelector('#board');
+const colors = [
+  '#00FFFF',
+  '#7FFF00',
+  '#FF69B4',
+  '#0000FF',
+  '#00BFFF',
+  '#7CFC00',
+  '#800000',
+  '#FF4500',
+];
+const SQUARES_NUMBER = 500;
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+
+  square.addEventListener('mouseleave', () => removeColor(square));
+
+  board.append(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = `0 0 2px #000`;
+}
+
+function getRandomColor() {
+  const index = Math.floor(Math.random() * colors.length);
+  return colors[index];
+}
